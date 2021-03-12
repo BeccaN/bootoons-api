ba50250703b1d966ff77f6a5183ef47e8cd8f211
rncongi <rncongi@gmail.com>
2021-03-12 17:34:43 -0500

    Created category serializer, added comic_category to comic serializer

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,4 +1,7 @@
 class Comic < ApplicationRecord
     belongs_to :category
 
+    def comic_category
+        self.category.name
+    end
 end
--- app/serializers/comic_serializer.rb
+++ app/serializers/comic_serializer.rb
@@ -1,4 +1,4 @@
 class ComicSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :img_url
+  attributes :id, :title, :description, :img_url, :comic_category
   # has_many :tags
 end
