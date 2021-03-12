b1365c20f92e3dad421111fe09d406bde923a900
rncongi <rncongi@gmail.com>
2021-03-12 15:06:53 -0500

    Added comic create route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :comics, only: [:index]
+  resources :comics, only: [:index, :create]
 end
